7f65ba1c759ec79e62fb3da3394ce147ab9aecbc
刘春牢 <liuchunlao@qq.com>
2016-03-22 17:46:55 +0800

    关于
    关于界面默认是不能滚动的！关闭滚动效果

--- WechatBook/Classes/Config/Controllers/MDRAboutController.m
+++ WechatBook/Classes/Config/Controllers/MDRAboutController.m
@@ -15,15 +15,18 @@
 
     [super viewDidLoad];
     
-    // logo
+    // 1.logo
     self.tableView.tableHeaderView = [MDRAboutHeaderView aboutHeaderView];
     
-    // 版权说明 需要适配不同屏幕调整高度
+    // 2.版权说明 需要适配不同屏幕调整高度
     UIView *footerView = [[[NSBundle mainBundle] loadNibNamed:@"MDRAboutFooterView" owner:nil options:nil] lastObject];
     
     footerView.height = [UIScreen mainScreen].bounds.size.height - self.tableView.tableHeaderView.height - 4 * 44 - 100;
     
     self.tableView.tableFooterView = footerView;
+    
+    // 3.此页面不能滚动
+    self.tableView.scrollEnabled = NO;
     
     
 }
